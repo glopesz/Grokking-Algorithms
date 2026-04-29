ade4d4a030c9cb88463e203a065a7e203b6ade76
glopesz <gl8109608@gmail.com>
2026-04-29 17:22:14 -0300

    apply naming conventions

--- selection_sort.py
+++ selection_sort.py
@@ -1,4 +1,4 @@
-def findSmallest(arr):
+def find_smallest(arr):
     smallest = arr[0]
     smallest_index = 0
     for i in range(1, len(arr)):
@@ -7,11 +7,11 @@ def findSmallest(arr):
             smallest_index = i
     return smallest_index
     
-def selectionSort(arr):
+def selection_sort(arr):
     newArr = []
     for i in range(len(arr)):
-        smallest = findSmallest(arr)
+        smallest = find_smallest(arr)
         newArr.append(arr.pop(smallest))
     return newArr
 
-print(selectionSort([5, 3, 6, 2, 10]))
+print(selection_sort([5, 3, 6, 2, 10]))
